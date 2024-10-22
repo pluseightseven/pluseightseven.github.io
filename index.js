02954f81f5f1660a7fa94047d5b161d2773ffb78
pluseightseven <pluseightseven@gmail.com>
2024-10-22 22:09:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const pes = document.createElement("h1");
 pes.setAttribute("id", "pes");
-pes.style.position = "absolute";
+//pes.style.position = "absolute";
 pes.textContent = "+87";
-pes.fontSize = "269%";
-pes.style.positionX = "50%";
-pes.style.positionY = "50%";
+pes.fontSize = "875%";
+//pes.style.positionX = "50%";
+//pes.style.positionY = "50%";
+document.body.style.width = window.screen.availWidth;
+document.body.style.height = window.screen.availHeight;
+document.body.alignSelf ="center";
+document.body.justifySelf = "center";
 document.body.appendChild(pes);
