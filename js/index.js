2e733c2ec96dbab1daa56951d5b44a4735946f19
pluseightseven <pluseightseven@gmail.com>
2024-10-23 00:02:11 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -2,13 +2,23 @@
 //const peshtml = document.querySelector("html");
 //peshtml.style.backgroundColor = "#000";
 export const baseURI = document.baseURI;
-const pesbody = document.body;
+
 const pestitle = document.querySelector("title");
 pestitle.textContent = "+87";
 pestitle.style.fontFamily = 'ANTON';
 
 const pesbody = document.body;
 const pesbodystyle = pesbody.style;
+pesbodystyle.backgroundColor = "#000";
+pesbodystyle.color = "#fff";
+pesbodystyle.width = window.screen.availWidth;
+pesbodystyle.height = window.screen.availHeight;
+pesbodystyle.alignSelf ="center";
+pesbodystyle.justifySelf = "center";
+pesbodystyle.alignContent ="center";
+pesbodystyle.justifyContent = "center";
+pesbodystyle.alignItems ="center";
+pesbodystyle.justifyItems = "center";
 
 const pes = document.createElement("h1");
 const pestyle = pes.style;
@@ -27,14 +37,4 @@ pestyle.justifySelf = "center";
 //pes.style.positionX = "50%";
 //pes.style.positionY = "50%";
 
-pesbodystyle.backgroundColor = "#000";
-pesbodystyle.color = "#fff";
-pesbodystyle.width = window.screen.availWidth;
-pesbodystyle.height = window.screen.availHeight;
-pesbodystyle.alignSelf ="center";
-pesbodystyle.justifySelf = "center";
-pesbodystyle.alignContent ="center";
-pesbodystyle.justifyContent = "center";
-pesbodystyle.alignItems ="center";
-pesbodystyle.justifyItems = "center";
 pesbody.appendChild(pes);
