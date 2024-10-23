e5ace2c8f86026fa060af9a0969bd65acf130210
pluseightseven <pluseightseven@gmail.com>
2024-10-23 22:26:30 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-const plus87 = document.createElement("main");
-plus87.textContent = "+87";
-document.body.appendChild(plus87);
-
 document.body.setAttribute("width", screen.availWidth);
 document.body.setAttribute("height", screen.availHeight);
 document.body.style.backgroundColor = "#290000";
@@ -12,25 +8,24 @@ document.body.style.padding = 0;
 document.body.style.alignItems = "center";
 document.body.style.justifyContent = "center";
 
-const docti = document.querySelector('title');
-docti.textContent = '+87 : Plus Eight Seven';
+const tabtitlep87 = document.querySelector('title');
+tabtitlep87.textContent = '+87 : Plus Eight Seven';
 
-const docma = document.createElement("main");
-docma.setAttribute("width", screen.availWidth);
-docma.setAttribute("height", screen.availHeight);
-docma.style.display = "flex";
-docma.style.alignItems = "center";
-docma.style.justifyContent = "center";
-docma.style.margin = 0;
-docma.style.padding = 0;
-docma.style.backgroundColor = "#000";
-docma.style.color = "#290260";
-// docma.style.fontSize = "300%";
-document.body.appendChild(docma);
+const plus87 = document.createElement("main");
+plus87.textContent = "+87";
+plus87.setAttribute("width", screen.availWidth);
+plus87.setAttribute("height", screen.availHeight);
+plus87.style.display = "flex";
+plus87.style.alignItems = "center";
+plus87.style.justifyContent = "center";
+plus87.style.margin = 0;
+plus87.style.padding = 0;
+plus87.style.backgroundColor = "#000";
+plus87.style.color = "#290260";
+plus87.style.fontSize = "300%";
+document.body.appendChild(plus87);
 
-const maidi = document.createElement("div");
-maidi.setAttribute("max-width", "600px");
+const sectionp87 = document.createElement("section");
+sectionp87.setAttribute("max-width", "600px");
 
-docma.appendChild(maidi);
-
-
+plus87.appendChild(sectionp87);
