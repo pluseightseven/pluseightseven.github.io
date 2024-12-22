1c5021387da6cb96307d7c1b26c925337761245b
pluseightseven <pluseightseven@gmail.com>
2024-12-22 21:41:02 +0530

    Update urls.py

--- pluseightseven/urls.py
+++ pluseightseven/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pluseightseven.github.io project.
+URL configuration for pluseightseven project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -26,8 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     distill_path('', views.home, name='home', distill_func=get_index),
     #path('', views.home, name='home'),
-    distill_path('pluseightseven.github.io/settings/', views.settings, name='settings', distill_func=get_index),
-    distill_path('pluseightseven.github.io/username/', views.username, name='username', distill_func=get_index),
-    distill_path('pluseightseven.github.io/account/', views.account, name='account', distill_func=get_index)
+    distill_path('pluseightseven/settings/', views.settings, name='settings', distill_func=get_index),
+    distill_path('pluseightseven/username/', views.username, name='username', distill_func=get_index),
+    distill_path('pluseightseven/account/', views.account, name='account', distill_func=get_index)
 ]
 
